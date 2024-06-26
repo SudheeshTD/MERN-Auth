d2d8ef0089d611da1f016dc58ce3c79688675f97
Sudheesh T D <sudheeshtd01@gmail.com>
2024-06-26 00:45:58 -0500

    error handling

---
+++ backend/controllers/userController.js
@@ -0,0 +1,15 @@
+import asyncHandler from "express-async-handler";
+
+//@Desc      Auth User/Set Token
+//route      POST /api/users/auth
+//@access    Public
+
+
+
+const authUser = asyncHandler(async (req,res) => {
+    res.status(401);
+    throw new Error('SSomething went wrong')
+    await res.status(200).json({message: "Auth User"})
+});
+
+export { authUser };
