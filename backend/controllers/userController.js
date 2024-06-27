32f6bf1ed07664856c8e2981f06bd9a6e6b2948f
Sudheesh T D <sudheeshtd01@gmail.com>
2024-06-26 19:14:05 -0500

    added login, logout, update , register controllers

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,13 +3,42 @@ import asyncHandler from "express-async-handler";
 //@Desc      Auth User/Set Token
 //route      POST /api/users/auth
 //@access    Public
-
-
-
 const authUser = asyncHandler(async (req,res) => {
-    res.status(401);
-    throw new Error('SSomething went wrong')
-    await res.status(200).json({message: "Auth User"})
+    res.status(200).json({message: "Auth User"})
 });
 
-export { authUser };
+//@Desc      Register User
+//route      POST /api/users
+//@access    Public
+const registerUser = asyncHandler(async (req,res) => {
+    res.status(200).json({message: "Register User"})
+});
+
+//@Desc      Logout User
+//route      POST /api/users/logout
+//@access    Public
+const logoutUser = asyncHandler(async (req,res) => {
+    res.status(200).json({message: "Logout User"})
+});
+
+//@Desc      get User Profile
+//route      GET /api/users/profile
+//@access    Private
+const getUserProfile = asyncHandler(async (req,res) => {
+    res.status(200).json({message: "User Profile"})
+});
+
+//@Desc      Update user Profile
+//route      PUT /api/users/profile
+//@access    Public
+const updateUserProfile = asyncHandler(async (req,res) => {
+    res.status(200).json({message: "Update User Profile"})
+});
+
+export { 
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
+ };
