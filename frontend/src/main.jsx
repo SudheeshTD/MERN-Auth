981b6ad76c561e8c1bd0fcffc15006d46347c80c
Sudheesh T D <sudheeshtd01@gmail.com>
2024-07-06 18:31:08 -0500

    added hero, in home screen n header

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route,
+  RouteProvider,
+} from 'react-router-dom'
 import App from './App.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path = '/' element = {App />}>
+
+    </Route>
+  )
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode> 
+    <RouteProvider router = { router } />
   </React.StrictMode>,
 )
