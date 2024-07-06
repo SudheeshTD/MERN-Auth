1a6db5ee64794f645ce1fe67c9b75a788d09a1b2
Sudheesh T D <sudheeshtd01@gmail.com>
2024-07-06 18:59:35 -0500

    added container to hero

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 import {
-  createBrowserRouter, 
+  createBrowserRouter,
   createRoutesFromElements,
   Route,
-  RouteProvider,
-} from 'react-router-dom'
-import App from './App.jsx'
+  RouterProvider,
+} from 'react-router-dom';
+import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css'
+import HomeScreen from './screens/HomeScreen';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path = '/' element = {App />}>
-
+    <Route path='/' element={<App />}>
+      <Route index={true} path='/' element={<HomeScreen />} />
     </Route>
   )
-)
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode> 
-    <RouteProvider router = { router } />
-  </React.StrictMode>,
-)
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
